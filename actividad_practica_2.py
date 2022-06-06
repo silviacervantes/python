b73d80964dffd109e4346f6f576815c964d058b1
Silvia Cervantes <silviaccp@gmail.com>
2022-06-06 13:12:59 -0300

    Update actividad_practica_2.py

--- actividad_practica_2.py
+++ actividad_practica_2.py
@@ -63,7 +63,7 @@ if es_fecha(dia,mes,anio): print(dia,"/",mes,"/",anio," :Fecha Valida")
 else: print(dia,"/",mes,"/",anio," :((!)) Fecha invalida")
 
 
-'''
+
 # 3) Un comercio de electrodomésticos necesita para su línea de cajas un programa que le indique al cajero el cambio que debe entregarle al cliente. Para eso se ingresan dos números enteros, correspondientes al total de la compra y al dinero recibido. Informar cuántos billetes de cada denominación deben ser entregados al cliente como vuelto, de tal forma que se minimice la cantidad de billetes. Considerar que existen billetes de $500, $100, $50, $20, $10, $5 y $1. Emitir un mensaje de error si el dinero recibido fuera insuficiente. Ejemplo: si la compra es de $317 y se abona con $500, el vuelto debe contener 1 billete de $100, 1 billete de $50, 1 billete de $20, 1 billete de $10 y 3 billetes de $1.
 
 
@@ -108,12 +108,72 @@ else: print(vuelto(importe_total, importe_entregado))
 
 
 
-'''
+
 # 4) Escribir dos funciones separadas para imprimir por pantalla los siguientes patrones de asteriscos, donde la cantidad de filas se recibe como parámetro:
+def funcion1(cant):
+    for i in range(1,cant+1,1):
+        print("*"*10)
+
+def funcion2(cant):
+    for i in range(2,cant+1,1):
+        print("*"*i)
+
+cant_filas = int(input("Ingrese la cantidad de filas:"))
+funcion1(cant_filas)
+funcion2(cant_filas)
+
+
 # 5) Crear una función lambda que devuelva el cuadrado de un valor recibido cómo parámetro. Desarrollar además un programa para verificar el comportamiento de la función.
+cuadrado = lambda x:x*x
+nro = int(input("Ingrese un valor para obtener su cuadrado: "))
+print("El cuadrado es:",cuadrado(nro))
+
 # 6) Crear una función lambda para comprobar si un número es par o impar. Desarrollar además un programa para verificar el comportamiento de la función.
+
+es_par = lambda nro:True if nro %2 ==0 else False
+numero = int(input("Ingrese un valor para determinar si es par o impar:"))
+if es_par(numero):
+    print("ES PAR")
+else:
+    print ("ES IMPAR")
+
 # 7) Crear una lista con los cuadrados de los números entre 1 y N (ambos incluidos), donde N se ingresa desde el teclado. Luego se solicita imprimir los últimos 10 valores de la lista.
+
+cuadrado = lambda x:x*x
+n=int(input("Ingrese la cantidad de elementos:"))
+lista=[]
+for i in range(1,n+1,1):
+    lista=lista+[(cuadrado(i))]
+longitud = len(lista)
+j=0
+while j<=10:
+    if longitud-1-j > 0:
+        print(lista[longitud-1-j])
+    j+=1
+
+'''
 # 8) Eliminar de una lista de palabras que se encuentren en una segunda lista. Imprimir la lista original, la lista de palabras a eliminar y la lista resultante.
+from turtle import clear
+
+
+listaA=['rojo','blanco','azul','verde','rosa','violeta','celeste','marron','negro','gris','naranja']
+listaB=['verde','naranja','azul']
+listaC=[]
+listaD=[]
+
+print("LISTA A: ",listaA)
+for i in range(0,len(listaB)-1,1):
+    elemento = listaB[i]
+    if elemento in listaA:
+        listaC = listaC.append(elemento)
+        listaA = listaA.remove(elemento)
+    else:
+        listaD = listaD.append(elemento)
+print(listaC)
+print(listaD)
+print(listaA)
+
+
 # 9) Escribir una función que reciba una lista como parámetro y devuelva True si la lista está ordenada en forma ascendente o False en caso contrario. Por ejemplo, ordenada([1, 2, 3]) retorna True y ordenada(['b', 'a']) retorna False. Desarrollar además un programa para verificar el comportamiento de la función.
 # 10) Desarrollar una función que determine si una cadena de caracteres es capicúa, sin utilizar cadenas auxiliares ni rebanadas. Escribir además un programa que permita verificar su funcionamiento.
 # 11) Leer una cadena de caracteres e imprimirla centrada en pantalla. Suponer que la misma tiene 80 columnas.
@@ -121,4 +181,3 @@ else: print(vuelto(importe_total, importe_entregado))
 # 13) Ingresar una frase desde el teclado y usar un conjunto para eliminar las palabras repetidas, dejando un solo ejemplar de cada una. Finalmente mostrar las palabras ordenadas según su longitud.
 # 14) Desarrollar una función eliminar_claves() que reciba como parámetros un diccionario y una lista de claves. La función debe eliminar del diccionario todas las claves contenidas en la lista, devolviendo el diccionario modificado y un valor de verdad que indique si la operación fue exitosa. Desarrollar también un programa para verificar su comportamiento.
 # 15) Escribir una función para eliminar una subcadena de una cadena de caracteres, a partir de una posición y cantidad de caracteres dados, devolviendo la cadena resultante. Escribir también un programa para verificar el comportamiento de la misma. Escribir una función utilizando rebanadas.
-'''
