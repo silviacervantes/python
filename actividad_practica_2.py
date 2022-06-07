212a53525066a562645937506476b9278de323de
Silvia Cervantes <silviaccp@gmail.com>
2022-06-07 10:05:55 -0300

    Update actividad_practica_2.py

--- actividad_practica_2.py
+++ actividad_practica_2.py
@@ -168,9 +168,25 @@ print("PALABRAS A ELIMINAR:",listaC)
 print("LISTA RESULTADO:",listaA)
 '''
 # 9) Escribir una función que reciba una lista como parámetro y devuelva True si la lista está ordenada en forma ascendente o False en caso contrario. Por ejemplo, ordenada([1, 2, 3]) retorna True y ordenada(['b', 'a']) retorna False. Desarrollar además un programa para verificar el comportamiento de la función.
+
+n=int(input("Ingrese cantidad de elementos"))
+# lista1 = []
+# lista2 = []
+# for i in range(0,n,1):
+#     lista1[i] = input("Elemento[",i,"]:")
+# lista2=lista1.sorted()
+# ordenada = lambda resultado: True if lista1 == lista2 else False
+# if ordenada: print("Esta ordenada.") 
+# else:print("Esta desordenada.")
+
 # 10) Desarrollar una función que determine si una cadena de caracteres es capicúa, sin utilizar cadenas auxiliares ni rebanadas. Escribir además un programa que permita verificar su funcionamiento.
+
 # 11) Leer una cadena de caracteres e imprimirla centrada en pantalla. Suponer que la misma tiene 80 columnas.
+
 # 12) Escribir una función que reciba como parámetro una tupla conteniendo una fecha (día,mes,año) y devuelva una cadena de caracteres con la misma fecha expresada en formato extendido. Por ejemplo, para (12, 10,17) devuelve “12 de Octubre de 2017”. Escribir también un programa para verificar su comportamiento.
+
 # 13) Ingresar una frase desde el teclado y usar un conjunto para eliminar las palabras repetidas, dejando un solo ejemplar de cada una. Finalmente mostrar las palabras ordenadas según su longitud.
+
 # 14) Desarrollar una función eliminar_claves() que reciba como parámetros un diccionario y una lista de claves. La función debe eliminar del diccionario todas las claves contenidas en la lista, devolviendo el diccionario modificado y un valor de verdad que indique si la operación fue exitosa. Desarrollar también un programa para verificar su comportamiento.
-# 15) Escribir una función para eliminar una subcadena de una cadena de caracteres, a partir de una posición y cantidad de caracteres dados, devolviendo la cadena resultante. Escribir también un programa para verificar el comportamiento de la misma. Escribir una función utilizando rebanadas.
+
+# 15) Escribir una función para eliminar una subcadena de una cadena de caracteres, a partir de una posición y cantidad de caracteres dados, devolviendo la cadena resultante. Escribir también un programa para verificar el comportamiento de la misma. Escribir una función utilizando rebanadas
